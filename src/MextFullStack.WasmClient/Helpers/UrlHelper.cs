4c4f2f7787a8371e25ef10892cee46d5531afee3
ALPER TUNGA <alper@altu.dev>
2024-05-09 14:45:05 +0300

    [Feature]: Blazor project has been created.
    * ProductsPage and Loading components created.

---
+++ src/MextFullStack.WasmClient/Helpers/UrlHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace MextFullStack.WasmClient.Helpers
+{
+    public class UrlHelper
+    {
+        public string ApiBaseUrl { get; set; }
+
+        public UrlHelper(string apiBaseUrl)
+        {
+            ApiBaseUrl = apiBaseUrl;
+        }
+    }
+}
---
+++ src/MextFullStack.WasmClient/Program.cs
@@ -0,0 +1,14 @@
+using MextFullStack.WasmClient;
+using MextFullStack.WasmClient.Helpers;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7010/api/") });
+
+builder.Services.AddSingleton<UrlHelper>(new UrlHelper("https://localhost:7010/api/"));
+
+await builder.Build().RunAsync();
