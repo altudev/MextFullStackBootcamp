c158af6d973ae0e54865161a8cd545adb32d266b
ALPER TUNGA <alper@altu.dev>
2024-06-11 10:58:21 +0300

    [Feature]: Blazor project created. TailwindCSS configuration applied.

---
+++ MextFullstackSaaS/MextFullstackSaaS.WasmClient/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./**/*.{razor,html,cshtml}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+      require('daisyui'),
+    ],
+  daisyui: {
+      themes: [
+          "dark",
+          "black",
+          "night",
+          "sunset"
+      ],
+  },
+}
+
